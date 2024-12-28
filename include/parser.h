e3aa6506236d5b745798a88e902d65345ba9e10d
Jitansh Arora <arorajitansh32@gmail.com>
2024-12-28 15:43:46 -0400

    Modified the parser header file

--- include/parser.h
+++ include/parser.h
@@ -2,16 +2,44 @@
 #define PARSER_H
 
 #include "token.h"
+#include "errors.h"
+
+typedef enum
+{
+    FUNCTION,
+    FUNCTION_LIST,
+    FUNCTION_PARAMETERS,
+    STATEMENT,
+    STATEMENT_LIST,
+    TYPE,
+    IDENTIFIER,
+    IDENTIFIER_DECLARATION,
+    IDENTIFIER_DEFINITION,
+    BOOL_VALUE,
+    CONDITIONAL,
+    OPTIONAL_ELSE,
+    EXPRESSION,
+    LOGICAL_EXPRESSION,
+    RELATIONAL_EXPRESSION,
+    ADDITIVE_EXPRESSION,
+    MULTIPLICATIVE_EXPRESSION,
+    TERM,
+    GPIO_OPERATION,
+    GPIO_VALUES,
+    GPIO_PIN,
+    WHILE_LOOP,
+    RETURN_STATEMENT
+} ASTNodeType;
 
 // Structure for AST nodes
 typedef struct ASTNode
 {
-    char *type;                // Node type (e.g., "expression", "statement")
+    ASTNodeType type;          // Node type (e.g., "expression", "statement")
     struct ASTNode **children; // Array of child nodes
     int num_children;          // Number of children
 } ASTNode;
 
 // Parser function
-ASTNode *parse_tokenstream(TokenStream *token_stream);
+extern ASTNode *parse_token_stream(TokenStream *token_stream, ErrorList *error_list);
 
 #endif
